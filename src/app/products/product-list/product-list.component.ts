6d868e16d16d74be91e95345d53ef0895e813247
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-10-12 20:54:45 +0200

    Import json file into component

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/interfaces/product';
+import myData from '../../../api/products/products.json';
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
@@ -9,28 +10,7 @@ export class ProductListComponent implements OnInit {
 
   pageTitle = `Product List`;
   listFilter = 'carrito';
-  products: Array<IProduct> = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2021",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "../../assets/images/leaf_rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "../../assets/images/garden_cart.png"
-    }
-  ];
+  products: Array<IProduct> = myData;
   showImage: boolean = false;
   filteredProducts: any;
   imageWidth = 50;

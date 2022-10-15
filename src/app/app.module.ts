71823ed840142cfc924a4c0549228c641ea00a0a
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-10-15 17:28:32 +0200

    Create some more c to play with router. Selector is out since won´t be a nested comp but a routing one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { ProductListComponent } from './products/product-list/product-list.compo
 import { FormsModule } from '@angular/forms';
 import { ConvertToSoacesPipe } from './shared/convert-to-soaces.pipe';
 import { StarComponent } from './shared/star/star.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSoacesPipe,
-    StarComponent
+    StarComponent,
+    WelcomeComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,

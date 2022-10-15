dbe48a0b66f2fb19792be849ca480400ddd6665b
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-10-15 19:01:34 +0200

    Product detail comp makes use of the getProduct method for the id passed in

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from 'src/app/interfaces/product';
+import { ProductServiceService } from 'src/app/shared/services/product-service.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,12 +11,25 @@ export class ProductDetailComponent implements OnInit {
 
   pageTitle = 'Product detail';
   product: IProduct | undefined;
+  errorMessage = '';
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private productService: ProductServiceService
+    ) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.getProduct(id);
     this.pageTitle += `: ${id}`
+  }
+
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
   }
 
   onBack(): void {
